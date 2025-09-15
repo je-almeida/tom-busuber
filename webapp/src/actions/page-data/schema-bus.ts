caa1e74195ac0d27a32bbad30c34fd54c71dc26b
je-almeida <jeff.ifsp@gmail.com>
2025-09-14 21:22:17 -0300

    feat: update fetchSchema revalidation time and add cache configuration

--- webapp/src/actions/page-data/schema-bus.ts
+++ webapp/src/actions/page-data/schema-bus.ts
@@ -4,7 +4,8 @@ import { Strapi } from "../../utils/strapi";
 export async function fetchSchema() {
   try {
     return await Strapi.public("config", {
-      next: { revalidate: 60 },
+      next: { revalidate: 120 },
+      cache: "force-cache",
     });
   } catch (error) {
     console.error("Error fetching schema:", error);
