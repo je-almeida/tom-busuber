29866ec9da1d7b564b8c7ac4ab8e87ed9737181e
je-almeida <jeff.ifsp@gmail.com>
2025-09-14 22:22:00 -0300

    fix: correct hostname for Golffox API in next.config.ts

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "sv.golffox.com.br",
+        hostname: "api.golffox.com.br",
         pathname: "/uploads/**",
       },
       {
